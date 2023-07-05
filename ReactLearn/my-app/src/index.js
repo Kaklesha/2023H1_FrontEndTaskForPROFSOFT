47b2f8234a46cf38b3742bcab7901253f68dd76f
kira <k-korni1ov@yandex.ru>
2023-07-05 16:05:37 +0400

    feat: base elem's

--- ReactLearn/my-app/src/index.js
+++ ReactLearn/my-app/src/index.js
@@ -2,16 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+//const elem = <h2>Hello world</h2>;
+//Для понимания v
+// const elem = React.createElement('h2',null,'Hello world');
+
+const text ='Hello world!';
+
+//const ff = new Date();
+
+//Если многострочное испл ()
+// Эдементы в контейнер оборачиваем
+//Стандартные св-ва через CamelCase class->className
+//Обьекты не выводятся через {"интерполяция"}
+
+const elem =(
+  <div>
+    <h2 className='fff' tabIndex={0}>Text: {text}</h2>
+      <input type="text" />
+     <button>Click</button>
+     <label htmlFor=""></label>
+     <button class='ddd' />
+    
+  </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  elem
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//
