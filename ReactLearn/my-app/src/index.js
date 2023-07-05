f83dc194572ae66068ec8d00905ac1ad8a747e25
kira <k-korni1ov@yandex.ru>
2023-07-05 19:42:26 +0400

    feat: done maket React proj

--- ReactLearn/my-app/src/index.js
+++ ReactLearn/my-app/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/app/app';
 
 
 //const elem = <h2>Hello world</h2>;
@@ -30,11 +30,14 @@ const text ='Hello world!';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
-  //elem
+  
+  <React.StrictMode>
+    
+  <App />
+    
+  </React.StrictMode>
 
 );
+//CRUD
 
-//
+//https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.1/js/bootstap.min.js
