ec757f16adb572c9abc2985cab2cbc58374ff5e5
kira <k-korni1ov@yandex.ru>
2023-07-06 16:58:33 +0400

    feat: add control properties

--- ReactLearn/my-app/src/components/emploees-add-form/employees-add-form.js
+++ ReactLearn/my-app/src/components/emploees-add-form/employees-add-form.js
@@ -1,18 +1,42 @@
 
 import './employees-add-form.css';
 
-const EmployeesAddForm = ()=>{
+import {Component} from "react";
+
+class EmployeesAddForm extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            name: '',
+            salary: 0
+        }
+    }
+
+
+    onValueChange=(e)=>{
+        this.setState(
+            {
+                //prop: e.target.value
+                [e.target.name]: e.target.value
+            }
+        )
+    }
+
+
+   render(){
+
+    const{name,salary} = this.state
     return(
         <div className="app-add-form">
             <h3>Добавьте нового сотрудника</h3>
             <form className="add-form d-flex">
                   <input type="text" 
                   className='form-control new-post-label'
-                  placeholder="Как его зовут"
-                  />
+                  placeholder="Как его зовут"  value={name} name="name"
+                  onChange={this.onValueChange}/>
                   <input type="number" 
                         className="form-control new-post-label" 
-                        placeholder="3\П в RUB" />
+                        placeholder="3\П в RUB" value={salary} name="salary" onChange={this.onValueChange}/>
                 
                   <button type="submit"
                         className="btn btn-outline-light">Добавить</button>
@@ -21,6 +45,7 @@ const EmployeesAddForm = ()=>{
 
         </div>
     )
+   }
 }
 
 export default EmployeesAddForm;
