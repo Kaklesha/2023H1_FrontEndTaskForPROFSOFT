fa0ff1daa09cab7d80df1cb2c0e68c77e62205c0
HNY_Badger <markelov-02@list.ru>
2023-07-06 16:39:03 +0400

    like feature

--- hb-app/src/components/employees-list-item/employees-list-item.js
+++ hb-app/src/components/employees-list-item/employees-list-item.js
@@ -1,27 +1,52 @@
 import "./employees-list-item.css";
+import { Component } from 'react';
 
-const EmployeesListItem = ({name, salary, increase}) => {
+class EmployeesListItem extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            increase: false,
+            isLike: false
+        }
+    }
 
-    const incr = increase ? " increase" : "";
+    incrSalary = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
 
-    return (
-        <li className={"list-group-item d-flex justify-content-between" + incr}>
-            <span className="list-group-item-label">{name}</span>
-            <input type='text' className="list-group-item-input" defaultValue={salary + "$"}/>
-            <div className="d-flex justify-content-center align-item-center">
-                <button type="button"
-                    className ="btn-cookie btn-sm">
-                    <i className='fas fa-cookie'></i>
-                </button>
+    setLike = () => {
+        this.setState(({isLiked}) => ({
+            isLiked: !isLiked
+        }))
+    }
 
-                <button type="button"
-                    className="btn-trash btn-sm">
-                    <i className='fas fa-trash'></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    render() {
+        const {name, salary} = this.props;
+        const {increase, isLiked} = this.state;
+        const incr = increase ? " increase" : "";
+        const like = isLiked ? " like" : "";
+    
+        return (
+            <li className={"list-group-item d-flex justify-content-between" + incr + like}>
+                <span onClick={this.setLike} className="list-group-item-label">{name}</span>
+                <input type='text' className="list-group-item-input" defaultValue={salary + "$"}/>
+                <div className="d-flex justify-content-center align-item-center">
+                    <button onClick={this.incrSalary} type="button"
+                        className ="btn-cookie btn-sm">
+                        <i className='fas fa-cookie'></i>
+                    </button>
+    
+                    <button type="button"
+                        className="btn-trash btn-sm">
+                        <i className='fas fa-trash'></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
--- test-app/src/App.js
+++ test-app/src/App.js
@@ -7,7 +7,8 @@ class WhoAmI extends Component {
     super(props);
     this.state = {
       years: 27,
-      text: "+"
+      text: "+",
+      position: ''
     }
   }
 
@@ -18,13 +19,24 @@ class WhoAmI extends Component {
     }))
   }
 
+  commitInputChanges = (e, color) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
   render() {
     const {name, surname, link} = this.props;
+    const {position, years} = this.state;
     return (
       <div>
         <button onClick={this.nextYear}>{this.state.text}</button>
-        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <h1>My name is {name}, surname - {surname}, age - {years}, position - {position}</h1>
         <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type='text' onChange={(e) => this.commitInputChanges(e, "some color")}/>
+        </form>
       </div>
     )
   }
@@ -34,7 +46,7 @@ function App() {
   return (
     <div className="App">
       <WhoAmI name="John" surname="Cena" link="facebook.com"/>
-      <WhoAmI name="Lu" surname="Wong" link="fuckingbitch.com"/>
+      <WhoAmI name="Ada" surname="Wong" link="fuckingbitch.com"/>
     </div>
   );
 }
