5d617a882c1f4cd9d6ec5868aadc4800cd809614
kira <k-korni1ov@yandex.ru>
2023-07-07 14:04:24 +0400

    fix: link stylesheet

--- ReactLearn/my-app/src/components/app-info/app-info.js
+++ ReactLearn/my-app/src/components/app-info/app-info.js
@@ -9,7 +9,7 @@ const AppInfo = (props) =>{
     return(
         <div className="app-info">
             <h1>Учёт сотрудников в компании Профсофт</h1>
-            <h2>Общее число сотрудников:{countEmployees}</h2>
+            <h2>Общее число сотрудников: {countEmployees}</h2>
             <h2>Премию получат:</h2>
         </div>
     );
