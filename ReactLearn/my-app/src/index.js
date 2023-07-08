ebbf471a6024c2078771cdbc1c589597a05f3ef2
Kaklesha <k-korni1ov@yandex.ru>
2023-07-08 16:10:23 +0400

    init: Go+React

--- ReactLearn/my-app/src/index.js
+++ ReactLearn/my-app/src/index.js
@@ -4,29 +4,8 @@ import './index.css';
 import App from './components/app/app';
 
 
-//const elem = <h2>Hello world</h2>;
-//Для понимания v
-// const elem = React.createElement('h2',null,'Hello world');
-
 const text ='Hello world!';
 
-//const ff = new Date();
-
-//Если многострочное испл ()
-// Эдементы в контейнер оборачиваем
-//Стандартные св-ва через CamelCase class->className
-//Обьекты не выводятся через {"интерполяция"}
-
-// const elem =(
-//   <div>
-//     <h2 className='fff' tabIndex={0}>Text: {text}</h2>
-//       <input type="text" />
-//      <button>Click</button>
-//      <label htmlFor=""></label>
-//      <button class='ddd' />
-    
-//   </div>
-// )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -40,4 +19,4 @@ root.render(
 );
 //CRUD
 
-//https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.1/js/bootstap.min.js
+//https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.1/js/bootstap.min.js``
