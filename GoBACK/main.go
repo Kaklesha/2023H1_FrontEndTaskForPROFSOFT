2620e2b2e667d81b595364d961785a542e322c89
kira <k-korni1ov@yandex.ru>
2023-07-10 20:24:52 +0400

    feat:add select conn React+Go+Mariadb

--- GoBACK/main.go
+++ GoBACK/main.go
@@ -5,13 +5,15 @@ package main
 import (
 	
 	"fmt"
-	// "log"
+	"log"
 	"net/http"
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var database *sql.DB
 
 type Article struct {
 	ID 		 int	    `json:"id"`
@@ -24,14 +26,33 @@ type Article struct {
 type Articles []Article
 
 func allAtricles(w http.ResponseWriter, r *http.Request) {
-	epm := Articles{
-		//Article{1, "Test Title", 10000, false, true},
-		 Article{ID: 5, Name: "Title", Salary: 18000, Increase: true, Like: false},
-		 Article{ID: 6, Name: "Guth", Salary: 21000, Increase: false, Like: true},
-		Article{ID: 7, Name: "Ivan", Salary: 35000, Increase: true, Like: false},
-		
+	// epm := Articles{
+	// 	//Article{1, "Test Title", 10000, false, true},
+	// 	 Article{ID: 5, Name: "Title", Salary: 18000, Increase: true, Like: false},
+	// 	 Article{ID: 6, Name: "Guth", Salary: 21000, Increase: false, Like: true},
+	// 	Article{ID: 7, Name: "Ivan", Salary: 35000, Increase: true, Like: false},
+	// }
+	
+	//db =	connectWithDB()
 
+	results, err := database.Query("SELECT id, name, salary, increase, rise FROM Employees ")
+
+	if err != nil{
+		panic(err.Error())
 	}
+		epm := Articles{}
+	for results.Next(){
+		var user Article
+		err = results.Scan(&user.ID, &user.Name, &user.Salary, &user.Increase,&user.Like)
+		if err != nil{
+			panic(err.Error())
+		}
+		epm =append(epm , user)
+	}
+	fmt.Println(epm)
+	fmt.Println("Successfully connected to MariaBD database")
+
+
 	fmt.Println(epm)
 	fmt.Println("Endpoint")
 	w.Header().Set("Accept","application/json")
@@ -56,68 +77,56 @@ func Router() *mux.Router {
 	return 	myRouter
 }
 
-func CountFromTable(){
+// func CountFromTable(){
 
-	db, err := sql.Open("mysql", "test_user:1410@/my_db")
+// 	db, err := sql.Open("mysql", "test_user:1410@/my_db")
 
-	count, err := db.Query("SELECT count(*) FROM Employees ")
+// 	count, err := database.Query("SELECT count(*) FROM Employees ")
 
-	if err != nil{
-		panic(err.Error())
-	}
+// 	if err != nil{
+// 		panic(err.Error())
+// 	}
 	
-	for count.Next(){
-	var inc int
-	err = count.Scan(&inc)
-	if err != nil{
-		panic(err.Error())
-	}
-	fmt.Println(inc)
-    }
+// 	for count.Next(){
+// 	var inc int
+// 	err = count.Scan(&inc)
+// 	if err != nil{
+// 		panic(err.Error())
+// 	}
+// 	fmt.Println(inc)
+//     }
 
-	//return inc 
+// 	//return inc 
+
+// }
+
+func connectWithDB(){
 
 }
 
 
 func main() {
-	// fmt.Println("Citizix - Hello World!")
-	// r := Router()
-	// log.Fatal(http.ListenAndServe(":9000", r))
 
 	fmt.Println("Check for connect DB")
-
 	//db, err := sql.Open("mysql", "test_user:1410@tcp(127.0.0.1:3306)/my_db")
 	db, err := sql.Open("mysql", "test_user:1410@/my_db")
 
 	if err != nil{
 		panic(err.Error())
 	}
+	database = db
 	defer db.Close()
 
-	results, err := db.Query("SELECT id, name, salary, increase, rise FROM Employees ")
-	if err != nil{
-		panic(err.Error())
-	}
 
-		epm := Articles{}
-
-	for results.Next(){
-		var user Article
-		err = results.Scan(&user.ID, &user.Name, &user.Salary, &user.Increase,&user.Like)
-		if err != nil{
-			panic(err.Error())
-		}
-		epm =append(epm , user)
-		
-
-		
-	}
-	fmt.Println(epm)
+	 fmt.Println("Citizix - Hello World!")
+	 r := Router()
+	 log.Fatal(http.ListenAndServe(":9000", r))
 
 
-	fmt.Println("Successfully connected to MariaBD database")
-	//fmt.Println("")
+
+	//connectWithDB()
+
+	
 
 }
 
