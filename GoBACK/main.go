1ef41fe45ee5eb06569863e6cae49eb557e4db48
kira <k-korni1ov@yandex.ru>
2023-07-11 15:31:05 +0400

    refac: conn Go+DB

--- GoBACK/main.go
+++ GoBACK/main.go
@@ -13,7 +13,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var database *sql.DB
+//var database *sql.DB
 
 type Article struct {
 	ID 		 int	    `json:"id"`
@@ -25,25 +25,33 @@ type Article struct {
 
 type Articles []Article
 
-func allAtricles(w http.ResponseWriter, r *http.Request) {
+
+
+func getArticles(w http.ResponseWriter, r *http.Request) {
 	// epm := Articles{
 	// 	//Article{1, "Test Title", 10000, false, true},
 	// 	 Article{ID: 5, Name: "Title", Salary: 18000, Increase: true, Like: false},
 	// 	 Article{ID: 6, Name: "Guth", Salary: 21000, Increase: false, Like: true},
 	// 	Article{ID: 7, Name: "Ivan", Salary: 35000, Increase: true, Like: false},
 	// }
-	
-	//db =	connectWithDB()
+	fmt.Println("Endpoint")
+	w.Header().Set("Accept","application/json")
+	w.Header().Set("Access-Control-Allow-Origin","*")
+
+	database :=	connectWithDB()
 
 	results, err := database.Query("SELECT id, name, salary, increase, rise FROM Employees ")
-
+	defer database.Close()
 	if err != nil{
 		panic(err.Error())
 	}
+
 		epm := Articles{}
 	for results.Next(){
 		var user Article
 		err = results.Scan(&user.ID, &user.Name, &user.Salary, &user.Increase,&user.Like)
+		
+		
 		if err != nil{
 			panic(err.Error())
 		}
@@ -54,15 +62,16 @@ func allAtricles(w http.ResponseWriter, r *http.Request) {
 
 
 	fmt.Println(epm)
-	fmt.Println("Endpoint")
-	w.Header().Set("Accept","application/json")
-	w.Header().Set("Access-Control-Allow-Origin","*")
+	
 	json.NewEncoder(w).Encode(epm)
 }
 
-// func testPostAtricles(w http.ResponseWriter, r *http.Request){
-// 	fmt.Fprintf(w, "TEst POST Endpoint Hi")
-// }
+func postArticles(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "TEst POST Endpoint Hi")
+
+
+
+}
 
 // func homePage(w http.ResponseWriter, r *http.Request) {
 // 	fmt.Fprintf(w, "Hmepage Endpoint Hit")
@@ -72,7 +81,8 @@ func Router() *mux.Router {
 
 	myRouter := mux.NewRouter()
 
-	myRouter.HandleFunc("/api/employees", allAtricles).Methods("GET","OPTIONS")
+	myRouter.HandleFunc("/api/employees", getArticles).Methods("GET","OPTIONS")
+	myRouter.HandleFunc("/api/employees", postArticles).Methods("POST","OPTIONS")
 
 	return 	myRouter
 }
@@ -100,31 +110,40 @@ func Router() *mux.Router {
 
 // }
 
-func connectWithDB(){
-
-}
-
-
-func main() {
-
+func connectWithDB() *sql.DB {
 	fmt.Println("Check for connect DB")
 	//db, err := sql.Open("mysql", "test_user:1410@tcp(127.0.0.1:3306)/my_db")
+
 	db, err := sql.Open("mysql", "test_user:1410@/my_db")
 
 	if err != nil{
 		panic(err.Error())
 	}
-	database = db
-	defer db.Close()
+	//database = db
+	
+	return db
+}
+
+
+func main() {
+
+	// fmt.Println("Check for connect DB")
+	// //db, err := sql.Open("mysql", "test_user:1410@tcp(127.0.0.1:3306)/my_db")
+	// db, err := sql.Open("mysql", "test_user:1410@/my_db")
+
+	// if err != nil{
+	// 	panic(err.Error())
+	// }
+	// database = db
+	// defer db.Close()
 
 
 	 fmt.Println("Citizix - Hello World!")
 	 r := Router()
 	 log.Fatal(http.ListenAndServe(":9000", r))
 
-
-
 	//connectWithDB()
+
 
 	
 
